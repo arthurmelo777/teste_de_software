875d94b703b937d1cfc616acfa4ab3ad865137e7
Arthur Melo <53615432+arthurmelo777@users.noreply.github.com>
2024-07-09 10:48:37 +0000

    Implementa teste e classe de Calculadora

--- calculadora/modelo.py
+++ calculadora/modelo.py
@@ -1,15 +1,41 @@
 class Calculadora:
     def __init__(self):
-        pass
+        self._a = 0
+        self._b = 0
+
+    @property
+    def a (self):
+        return self._a
+    
+    @property
+    def b (self):
+        return self._b
+
+    def verifica_numero(self, a, b):
+        if type(a) is not int:
+            raise ValueError("'a' não é um número.")
+        if type(b) is not int:
+            raise ValueError("'b' não é um número.")
+
+        self.a = a
+        self.b = b
+
+        return True
 
     def adicao(self, a, b):
-        return a + b
+        if self.verifica_numero(a, b):
+            return a + b
 
     def subtracao(self, a, b):
-        return a - b
+        if self.verifica_numero(a, b):
+            return a - b
 
     def multiplicacao(self, a, b):
-        return a * b
+        if self.verifica_numero(a, b):
+            return a * b
 
     def divisao(self, a, b):
-        return a // b
+        if self.verifica_numero(a, b):
+            if (self._b == 0):
+                raise ValueError("Divisor deve ser diferente de 0.")
+            return a // b
--- calculadora/test.py
+++ calculadora/test.py
@@ -2,4 +2,66 @@ import unittest
 from modelo import Calculadora
 
 class CalculadoraTeste (unittest.TestCase):
-    
+    def test_verifica_a_eh_numero(self):
+        c = Calculadora()
+        with self.assertRaises(ValueError):
+            c.verifica_numero('a', 5)
+
+    def test_verifica_b_eh_numero(self):
+        c = Calculadora()
+        with self.assertRaises(ValueError):
+            c.verifica_numero(5, 'b')
+
+    def test_verifica_atribuicao(self):
+        c = Calculadora()
+        self.assertEqual(c.verifica_numero(2, 4), True)
+        self.assertEqual(c.a, 2)
+        self.assertEqual(c.b, 4)
+
+    def test_adicao_pass(self):
+        c = Calculadora()
+        r = c.adicao(2, 4)
+        self.assertEqual(r, 6)
+
+    def test_adicao_fail(self):
+        c = Calculadora()
+        r = c.adicao(2, 4)
+        self.assertNotEqual(r, 5)
+
+    def test_subtracao_pass(self):
+        c = Calculadora()
+        r = c.subtracao(2, 4)
+        self.assertEqual(r, -2)
+
+    def test_subtracao_fail(self):
+        c = Calculadora()
+        r = c.subtracao(2, 4)
+        self.assertNotEqual(r, 0)
+
+    def test_multiplicacao_pass(self):
+        c = Calculadora()
+        r = c.multiplicacao(2, 4)
+        self.assertEqual(r, 8)
+
+    def test_multiplicacao_fail(self):
+        c = Calculadora()
+        r = c.multiplicacao(2, 4)
+        self.assertNotEqual(r, 9)
+
+    def test_divisao_por_zero(self):
+        c = Calculadora()
+        with self.assertRaises(ValueError):
+            c.divisao(5, 0)
+
+    def test_divisao_pass(self):
+        c = Calculadora()
+        r = c.divisao(5, 2)
+        self.assertEqual(r, 2)
+
+    def test_divisao_fail(self):
+        c = Calculadora()
+        r = c.divisao(5, 2)
+        self.assertNotEqual(r, 10)
+
+if __name__ == '__main__':
+    unittest.main()
